f28f74973a7633348a6d89282d6bdef2f28de220
Caedman Ziwen Lan <lanziwen@apitable.com>
2023-11-27 10:50:34 +0800

    fix: fix style

--- src/components/setting/filter_select.tsx
+++ src/components/setting/filter_select.tsx
@@ -1,10 +1,15 @@
-import { LinkButton, useThemeColors, Modal, Button } from "@apitable/components"
-import { AddOutlined, FilterOutlined } from "@apitable/icons"
-import { Filter, t } from "@apitable/widget-sdk"
-import styled from 'styled-components';
-import { Strings } from '../../utils';
+import {
+  LinkButton,
+  useThemeColors,
+  Modal,
+  Button,
+} from "@apitable/components";
+import { AddOutlined, FilterOutlined } from "@apitable/icons";
+import { Filter, t } from "@apitable/widget-sdk";
+import styled from "styled-components";
+import { Strings } from "../../utils";
 
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
 
 interface IFilterSelect {
   value: any;
@@ -44,10 +49,14 @@ const FilterModal = ({ value, visible, onCancel, onConfirm }) => {
     const prevLength = filter ? (Object.values(filter)[0] as [])?.length : 0;
     const isAddFilter = curLength > prevLength;
     setFilter(value);
-    isAddFilter && setTimeout(() => {
-      if (!listRef.current) return;
-      listRef.current.scroll({ top: listRef.current.scrollHeight, behavior: 'smooth' });
-    });
+    isAddFilter &&
+      setTimeout(() => {
+        if (!listRef.current) return;
+        listRef.current.scroll({
+          top: listRef.current.scrollHeight,
+          behavior: "smooth",
+        });
+      });
   };
 
   return (
@@ -61,14 +70,11 @@ const FilterModal = ({ value, visible, onCancel, onConfirm }) => {
       width={800}
       centered
     >
-      <div ref={listRef} style={{ maxHeight: 440, overflowY: 'auto' }}>
-        <Filter 
-          filter={filter} 
-          onChange={onChange}
-        />
+      <div ref={listRef} style={{ maxHeight: 440, overflowY: "auto" }}>
+        <Filter filter={filter} onChange={onChange} />
       </div>
     </Modal>
-  )
+  );
 };
 
 export const FilterSelect = ({ value, onChange }: IFilterSelect) => {
@@ -78,36 +84,37 @@ export const FilterSelect = ({ value, onChange }: IFilterSelect) => {
   const onConfirm = (filter) => {
     onChange?.(filter);
     setShowModal(false);
-  }
+  };
 
   const filterLen = value ? value[Object.keys(value)[0]]?.length : 0;
 
   return (
     <div>
-      {
-        filterLen ?
-          <Button
-            size={'small'}
-            variant="jelly"
-            color="primary"
-            prefixIcon={<FilterOutlined />}
-            onClick={() => setShowModal(true)}
-            style={{ fontSize: 13 }}
-          >
-            {filterLen}{t(Strings.filters_amount)}
-          </Button> :
-          <AddFilterButton
-            href="javascript:void(0)"
-            color={colors.textCommonPrimary}
-            underline={false}
-            prefixIcon={<AddOutlined color={colors.textCommonPrimary} size={12} />}
-            onClick={() => setShowModal(true)}
-          >
-            <span style={{ fontSize: 12 }}>
-              {t(Strings.add_filter)}
-            </span>
-          </AddFilterButton>
-      }
+      {filterLen ? (
+        <Button
+          size={"small"}
+          variant="jelly"
+          color="primary"
+          prefixIcon={<FilterOutlined />}
+          onClick={() => setShowModal(true)}
+          style={{ fontSize: 13 }}
+        >
+          {filterLen}
+          {t(Strings.filters_amount)}
+        </Button>
+      ) : (
+        <AddFilterButton
+          href="javascript:void(0)"
+          color={colors.textCommonPrimary}
+          underline={false}
+          prefixIcon={
+            <AddOutlined color={colors.textCommonPrimary} size={12} />
+          }
+          onClick={() => setShowModal(true)}
+        >
+          <span style={{ fontSize: 12 }}>{t(Strings.add_filter)}</span>
+        </AddFilterButton>
+      )}
       <FilterModal
         visible={showModal}
         onCancel={() => setShowModal(false)}
@@ -115,5 +122,5 @@ export const FilterSelect = ({ value, onChange }: IFilterSelect) => {
         value={value}
       />
     </div>
-  )
+  );
 };
